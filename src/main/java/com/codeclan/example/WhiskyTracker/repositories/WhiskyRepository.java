ca517007f6f3bff4d2ced45f8f0458035ce4972a
Gabriel Neil <gabrielneil@msn.com>
2021-06-02 23:24:40 +0100

    mvp

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository.java
@@ -0,0 +1,18 @@
+package com.codeclan.example.WhiskyTracker.repositories;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface WhiskyRepository extends JpaRepository<Whisky, Long> {
+
+    List<Whisky> findWhiskiesByAge(int age);
+
+    List<Whisky> findWhiskiesByDistillery(String distillery);
+
+    List<Whisky> findWhiskiesByAgeAndDistillery(int age, String distillery);
+
+}
